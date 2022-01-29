b001c04890af0d4a214ca3869926865b1f2769fd
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2022-01-30 01:21:23 +0530

    Added solution - LeetHub

---
+++ Sort by Set Bit Count - GFG/sort-by-set-bit-count.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    void sortBySetBitCount(int arr[], int n)
+    {
+        // Your code goes here
+        stable_sort(arr, arr+n, [](const int &a, const int &b){
+             return __builtin_popcount(a) > __builtin_popcount(b);
+        });
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        ob.sortBySetBitCount(arr, n);
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
