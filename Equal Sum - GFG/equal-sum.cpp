fcbdf0295c2210b5d26401d912d8b79e7c374685
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2022-02-17 02:23:51 +0530

    Added solution - LeetHub

---
+++ Equal Sum - GFG/equal-sum.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    string equilibrium(int *arr, int n) {
+        // code here
+        int rightSum = 0, leftSum = 0;
+
+        for(int i=0; i<n; i++)
+            rightSum += arr[i];
+    
+        for(int i=0; i<n; i++) {
+            rightSum -= arr[i];
+    
+            if(rightSum == leftSum)
+                return "YES";
+    
+            leftSum += arr[i];
+        }
+    
+        return "NO";
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.equilibrium(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
