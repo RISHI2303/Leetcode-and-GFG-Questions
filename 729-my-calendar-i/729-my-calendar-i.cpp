e4ab296b951eaa98276cf70f43a648e119c54c53
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2022-08-03 23:47:03 +0530

    Time: 170 ms (49.44%), Space: 38.7 MB (60.92%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,17 @@
+class MyCalendar {
+    map<int, int> books;
+public:
+    bool book(int s, int e) {
+        auto next = books.lower_bound(s); // first element with key not go before k (i.e., either it is equivalent or goes after).
+        if (next != books.end() && next->first < e) return false; // a existing book started within the new book (next)
+        if (next != books.begin() && s < (--next)->second) return false; // new book started within a existing book (prev)
+        books[s] = e;
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
