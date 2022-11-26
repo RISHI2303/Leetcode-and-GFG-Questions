8f95f6176e85ece9ad2de0c5d61b477e01e978d2
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2022-11-26 21:50:31 +0530

    Added solution - LeetHub

---
+++ Non-Repeating Element - GFG/non-repeating-element.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    int n = Integer.parseInt(br.readLine());
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Check obj = new Check();
+		    
+		    System.out.println(obj.firstNonRepeating(arr,n));
+		        
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Check{
+    public int firstNonRepeating(int[] arr, int n) {
+        // Complete the function
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for (int x : arr)
+            m.put(x, m.getOrDefault(x, 0) + 1);
+
+        for (int x : arr)
+            if (m.get(x) == 1)
+                return x;
+
+        return -1;
+    }  
+    
+}
+
