413e4a54e5ce289306187cfa76cf7e96cf54beb1
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2022-11-30 23:10:55 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (40.60%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& v) {
-        unordered_map <int, int> m;
-        unordered_set <int> s;
-    
-        for(int i: v) {
-            m[i]++;
+    bool uniqueOccurrences(vector<int>& arr) {
+        // Store the frequency of elements in the unordered map.
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
         }
-    
-        for(auto i: m) {
-            if(s.find(i.second) == s.end())
-                s.insert(i.second);
-            else
-                return false;
+        
+        // Store the frequency count of elements in the unordered set.
+        unordered_set<int> freqSet;
+        for (auto [key, value] : freq) {
+            freqSet.insert(value);
         }
-    
-        return true;
+        
+        // If the set size is equal to the map size, 
+        // It implies frequency counts are unique.
+        return freqSet.size() == freq.size();
     }
 };
