050f3228ff9d3927fddec04f98b77d3e87a06951
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2023-10-16 18:37:24 +0530

    Time: 2 ms (41.13%), Space: 6.5 MB (63.89%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int r) {
+        vector<int>v(r+1);
+        long temp=1;
+        v[0]=v[r]=1;
+        for(int i=1,up=r,down=1;i<r;i++,up--,down++){
+            temp = temp*up/down;
+            v[i]=temp;
+        }
+        return v;
+    }
+};
