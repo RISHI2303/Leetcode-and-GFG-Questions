e1793d30815a1abe6e06af53ae545fb7d792a34e
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-03-26 21:27:41 +0530

    Time: 43 ms (62.28%), Space: 43.5 MB (81.44%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Use cycle sort to place positive elements smaller than n
+        // at the correct index
+        int i = 0;
+        while (i < n) {
+            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+
+        // Iterate through nums
+        // return smallest missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all elements are at the correct index
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+};
