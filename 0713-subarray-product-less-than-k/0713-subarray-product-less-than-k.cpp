39680dca5a3a07038cf75252ffbe6e9a0f5a1741
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-03-27 14:42:45 +0530

    Time: 87 ms (16.24%), Space: 68.9 MB (13.98%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k == 0) return 0;
+        double logK = log(k);
+        int m = nums.size() + 1;
+        vector<double> logsPrefixSum(m);
+
+        // Calculate prefix sum of logarithms of elements
+        for (int i = 0; i < nums.size(); i++) {
+            logsPrefixSum[i + 1] = logsPrefixSum[i] + log(nums[i]);
+        }
+
+        int totalCount = 0;
+        // Calculate subarray count with product less than k
+        for (int currIdx = 0; currIdx < m; currIdx++) {
+            int low = currIdx + 1, high = m;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                if (logsPrefixSum[mid] < logsPrefixSum[currIdx] + logK - 1e-9) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            totalCount += low - currIdx - 1;
+        }
+        return totalCount;
+    }
+};
