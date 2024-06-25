e59a619cb8f81f8a0ccdc654b46227430b0a56c5
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-06-25 13:46:17 +0530

    Time: 2 ms (49.63%), Space: 10.1 MB (77.24%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        TreeNode* node = root;
+
+        while (node != nullptr) {
+            if (node->right == nullptr) {
+                sum += node->val;
+                node->val = sum;
+                node = node->left;
+            }
+            else {
+                TreeNode* succ = getSuccessor(node);
+                if (succ->left == nullptr) {
+                    succ->left = node;
+                    node = node->right;
+                }
+                else {
+                    succ->left = nullptr;
+                    sum += node->val;
+                    node->val = sum;
+                    node = node->left;
+                }
+            }
+        }
+
+        return root;
+    }
+
+private:
+    TreeNode* getSuccessor(TreeNode* node) {
+        TreeNode* succ = node->right;
+        while (succ->left != nullptr && succ->left != node) {
+            succ = succ->left;
+        }
+        return succ;
+    }
+};
