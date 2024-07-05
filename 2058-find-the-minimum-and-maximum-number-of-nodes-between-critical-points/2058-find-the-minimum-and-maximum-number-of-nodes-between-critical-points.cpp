3fef8561b2d6f2a1b0c1a66ca928b71e4fb8cdae
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-07-05 14:20:24 +0530

    Time: 146 ms (95.60%), Space: 115.8 MB (79.68%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> result = {-1, -1};
+
+        int minDistance = INT_MAX;
+
+        ListNode* previousNode = head;
+        ListNode* currentNode = head->next;
+        int currentIndex = 1;
+        int previousCriticalIndex = 0;
+        int firstCriticalIndex = 0;
+
+        while (currentNode->next != nullptr) {
+            if ((currentNode->val < previousNode->val &&
+                 currentNode->val < currentNode->next->val) ||
+                (currentNode->val > previousNode->val &&
+                 currentNode->val > currentNode->next->val)) {
+                if (previousCriticalIndex == 0) {
+                    previousCriticalIndex = currentIndex;
+                    firstCriticalIndex = currentIndex;
+                } else {
+                    minDistance =
+                        min(minDistance, currentIndex - previousCriticalIndex);
+                    previousCriticalIndex = currentIndex;
+                }
+            }
+
+            currentIndex++;
+            previousNode = currentNode;
+            currentNode = currentNode->next;
+        }
+
+        if (minDistance != INT_MAX) {
+            int maxDistance = previousCriticalIndex - firstCriticalIndex;
+            result = {minDistance, maxDistance};
+        }
+
+        return result;
+    }
+};
