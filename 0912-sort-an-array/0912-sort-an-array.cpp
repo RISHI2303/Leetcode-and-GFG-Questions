d30ff3e15021a1a221e4c7399a23519c146c9257
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-07-25 19:43:34 +0530

    Time: 99 ms (78.39%), Space: 102.6 MB (44.28%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,15 +1,12 @@
 class Solution {
-    // Bucket sort function for each place value digit.
     void bucketSort(vector<int>& arr, int placeValue) {
         vector<vector<int>> buckets(10, vector<int>());
-        // Store the respective number based on its digit.
         for (int& val : arr) {
             int digit = abs(val) / placeValue;
             digit = digit % 10;
             buckets[digit].push_back(val);
         }
 
-        // Overwrite 'arr' in sorted order of current place digits.
         int index = 0;
         for (int digit = 0; digit < 10; ++digit) {
             for (int& val : buckets[digit]) {
@@ -19,9 +16,7 @@ class Solution {
         }
     }
     
-    // Radix sort function.
     void radixSort(vector<int>& arr) {
-        // Find the absolute maximum element to find max number of digits.
         int maxElement = arr[0];
         for (int& val : arr) {
             maxElement = max(abs(val), maxElement);
@@ -32,14 +27,12 @@ class Solution {
             maxElement /= 10;
         }
 
-        // Radix sort, least significant digit place to most significant.
         int placeValue = 1;
         for (int digit = 0; digit < maxDigits; ++digit) {
             bucketSort(arr, placeValue);
             placeValue *= 10;
         }
 
-        // Seperate out negatives and reverse them. 
         vector<int> negatives, positives;
         for (int& val : arr) {
             if (val < 0) {
@@ -49,7 +42,6 @@ class Solution {
             }
         }
         reverse(negatives.begin(), negatives.end());
-        // Final 'arr' will be 'negative' elements, then 'positive' elements.
         merge(negatives.begin(), negatives.end(), positives.begin(), positives.end(), arr.begin());
     }
 
