8d4c92fb5ba8655af74838ae0dab0d45422e965a
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-09-13 19:48:54 +0530

    Time: 51 ms (86.85%), Space: 41.7 MB (72.15%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> result;
+
+        for (int i = 1; i < arr.size(); ++i) {
+            arr[i] ^= arr[i - 1];
+        }
+
+        for (const auto& q : queries) {
+            if (q[0] > 0) {
+                result.push_back(arr[q[0] - 1] ^ arr[q[1]]);
+            } else {
+                result.push_back(arr[q[1]]);
+            }
+        }
+
+        return result;
+    }
+};
