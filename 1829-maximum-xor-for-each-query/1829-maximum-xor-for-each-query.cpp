9ad5cd029cbef32f24bba78155efdc04ee576c34
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-11-08 13:47:20 +0530

    Time: 4 ms (57.14%), Space: 95.3 MB (92.93%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int xorProduct = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            xorProduct = xorProduct ^ nums[i];
+        }
+        vector<int> ans(nums.size());
+
+        int mask = (1 << maximumBit) - 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans[i] = xorProduct ^ mask;
+            xorProduct = xorProduct ^ nums[nums.size() - 1 - i];
+        }
+
+        return ans;
+    }
+};
