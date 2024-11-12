96f71d4caedcacb8b904676421c4798914859180
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-11-12 23:59:31 +0530

    Time: 55 ms (68.85%), Space: 99.1 MB (36.49%) - LeetHub

---
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items,
+                              vector<int>& queries) {
+        vector<int> ans(queries.size());
+        sort(items.begin(), items.end(),
+             [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; });
+
+        vector<vector<int>> queriesWithIndices(queries.size(), vector<int>(2));
+
+        for (int i = 0; i < queries.size(); i++) {
+            queriesWithIndices[i][0] = queries[i];
+            queriesWithIndices[i][1] = i;
+        }
+
+        sort(queriesWithIndices.begin(), queriesWithIndices.end(),
+             [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; });
+
+        int itemIndex = 0;
+        int maxBeauty = 0;
+
+        for (int i = 0; i < queries.size(); i++) {
+            int query = queriesWithIndices[i][0];
+            int originalIndex = queriesWithIndices[i][1];
+
+            while (itemIndex < items.size() && items[itemIndex][0] <= query) {
+                maxBeauty = max(maxBeauty, items[itemIndex][1]);
+                itemIndex++;
+            }
+
+            ans[originalIndex] = maxBeauty;
+        }
+        return ans;
+    }
+};
