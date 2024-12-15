a51db5f51df7130053ab723303282d65110ec96c
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2024-12-16 02:45:04 +0530

    Time: 324 ms (68.60%), Space: 97.9 MB (49.61%) - LeetHub

---
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        auto calculateGain = [](int passes, int totalStudents) {
+            return (double)(passes + 1) / (totalStudents + 1) -
+                   (double)passes / totalStudents;
+        };
+
+        priority_queue<pair<double, pair<int, int>>> maxHeap;
+        for (const auto& singleClass : classes) {
+            maxHeap.push({calculateGain(singleClass[0], singleClass[1]),
+                          {singleClass[0], singleClass[1]}});
+        }
+
+        while (extraStudents--) {
+            auto [currentGain, classInfo] = maxHeap.top();
+            maxHeap.pop();
+            int passes = classInfo.first;
+            int totalStudents = classInfo.second;
+            maxHeap.push({calculateGain(passes + 1, totalStudents + 1),
+                          {passes + 1, totalStudents + 1}});
+        }
+
+        double totalPassRatio = 0;
+        while (!maxHeap.empty()) {
+            auto [_, classInfo] = maxHeap.top();
+            maxHeap.pop();
+            totalPassRatio += (double)classInfo.first / classInfo.second;
+        }
+
+        return totalPassRatio / classes.size();
+    }
+};
