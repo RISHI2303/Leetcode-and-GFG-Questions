5886f5d9bdac9c937b0015cc086085013e498ef8
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2025-02-01 15:12:01 +0530

    Time: 59 ms (17.65%), Space: 66.4 MB (18.47%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit, vector<bool>& inStack) {
+        if (inStack[node]) {
+            return true;
+        }
+        if (visit[node]) {
+            return false;
+        }
+        visit[node] = true;
+        inStack[node] = true;
+        for (auto neighbor : adj[node]) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+        inStack[node] = false;
+        return false;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+
+        for (int i = 0; i < n; i++) {
+            for (auto node : graph[i]) {
+                adj[i].push_back(node);
+            }
+        }
+
+        vector<bool> visit(n), inStack(n);
+
+        for (int i = 0; i < n; i++) {
+            dfs(i, adj, visit, inStack);
+        }
+
+        vector<int> safeNodes;
+        for (int i = 0; i < n; i++) {
+            if (!inStack[i]) {
+                safeNodes.push_back(i);
+            }
+        }
+        return safeNodes;
+    }
+};
