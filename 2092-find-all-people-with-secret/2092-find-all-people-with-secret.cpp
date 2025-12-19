d7a439a12b0cc18de9b1bfc8c6a576aff10abb14
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2025-12-20 01:47:49 +0530

    Time: 219 ms (49.19%), Space: 207.2 MB (38.86%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -5,7 +5,6 @@ private:
 
 public:
     UnionFind(int n) {
-        // Initialize parent and rank arrays
         parent.resize(n);
         rank.resize(n);
         for (int i = 0; i < n; ++i) {
@@ -14,7 +13,6 @@ public:
     }
 
     int find(int x) {
-        // Find parent of node x. Use Path Compression
         if (parent[x] != x) {
             parent[x] = find(parent[x]);
         }
@@ -22,11 +20,9 @@ public:
     }
 
     void unite(int x, int y) {
-        // Unite two nodes x and y, if they are not already united
         int px = find(x);
         int py = find(y);
         if (px != py) {
-            // Union by Rank Heuristic
             if (rank[px] > rank[py]) {
                 parent[py] = px;
             } else if (rank[px] < rank[py]) {
@@ -39,12 +35,10 @@ public:
     }
 
     bool connected(int x, int y) {
-        // Check if two nodes x and y are connected or not
         return find(x) == find(y);
     }
 
     void reset(int x) {
-        // Reset the initial properties of node x
         parent[x] = x;
         rank[x] = 0;
     }
@@ -53,41 +47,32 @@ public:
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        // Sort meetings in increasing order of time
         sort(meetings.begin(), meetings.end(), [](auto& a, auto& b) {
             return a[2] < b[2];
         });
 
-        // Group Meetings in increasing order of time
         map<int, vector<pair<int, int>>> sameTimeMeetings;
         for (auto& meeting : meetings) {
             int x = meeting[0], y = meeting[1], t = meeting[2];
             sameTimeMeetings[t].emplace_back(x, y);
         }
     
-        // Create graph
         UnionFind graph(n);
         graph.unite(firstPerson, 0);
 
-        // Process in increasing order of time
         for (auto& [t, meetings] : sameTimeMeetings) {
-            // Unite two persons taking part in a meeting
             for (auto& [x, y] : meetings) {
                 graph.unite(x, y);
             }
             
-            // If any one knows the secret, both will be connected to 0.
-            // If no one knows the secret, then reset.
             for (auto& [x, y] : meetings) {
                 if (!graph.connected(x, 0)) {
-                    // No need to check for y since x and y were united
                     graph.reset(x);
                     graph.reset(y);
                 }
             }
         }
         
-        // Al those who are connected to 0 will know the secret
         vector<int> ans;
         for (int i = 0; i < n; ++i) {
             if (graph.connected(i, 0)) {
