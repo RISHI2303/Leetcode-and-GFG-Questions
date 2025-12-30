535546de5c5b1bb358f6d163592d17aa78bea118
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2025-12-30 20:10:56 +0530

    Time: 2 ms (21.58%), Space: 12.9 MB (6.69%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int row = 0; row + 2 < m; row++) {
+            for (int col = 0; col + 2 < n; col++) {
+                if (isMagicSquare(grid, row, col)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+private:
+    bool isMagicSquare(vector<vector<int>>& grid, int row, int col) {
+        string sequence = "2943816729438167";
+        string sequenceReversed = "7618349276183492";
+
+        string border = "";
+        vector<int> borderIndices = {0, 1, 2, 5, 8, 7, 6, 3};
+        for (int i : borderIndices) {
+            int num = grid[row + i / 3][col + (i % 3)];
+            border += to_string(num);
+        }
+
+        return (grid[row][col] % 2 == 0 && grid[row + 1][col + 1] == 5 &&
+                (sequence.find(border) != string::npos ||
+                 sequenceReversed.find(border) != string::npos));
+    }
+};
