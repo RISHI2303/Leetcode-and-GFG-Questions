9a977467bcd4c6e3979e6567ac6d743d633527d4
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2026-02-25 23:34:04 +0530

    Time: 3 ms (47.07%), Space: 14.3 MB (43.16%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
+    static int findWeight(int num) {
+        int weight = 0;
+        
+        while (num > 0) {
+            weight++;
+            num &= (num - 1);
+        }
+        
+        return weight;
+    }
+    
+    static bool compare(int a, int b) {
+        if (findWeight(a) == findWeight(b)) {
+            return a < b;
+        }
+        
+        return findWeight(a) < findWeight(b);
+    }
+    
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [](const int &a, const int &b){
-            int countA = __builtin_popcount(a); // to find the number of set bits of a in binary form
-            int countB = __builtin_popcount(b); // to find the number of set bits of b in binary form
-            if(countA == countB) // if both the numbers have same no of set bits
-                return (a < b); // if a < b (in decimal form), then return true and put a before b else vice versa
-            else
-                return (countA < countB); // if countA < countB, then return true and put a before b else vice versa
-        });
-
-        return arr; // since reference of this vector was passed as parameter, all the changes are done in the original vector only.
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
     }
 };
