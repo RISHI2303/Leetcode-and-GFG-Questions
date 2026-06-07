695fa4d95b818264facd2173d94dc7cbba06812e
Rishi Goyal <53977317+RISHI2303@users.noreply.github.com>
2026-06-08 01:34:23 +0530

    Time: 70 ms (36.8%), Space: 84.2 MB (71.73%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> nodeMap = new HashMap<>();
+
+        Set<Integer> children = new HashSet<>();
+
+        for (int[] description : descriptions) {
+            int parentValue = description[0];
+            int childValue = description[1];
+            boolean isLeft = description[2] == 1;
+
+            if (!nodeMap.containsKey(parentValue)) {
+                nodeMap.put(parentValue, new TreeNode(parentValue));
+            }
+            if (!nodeMap.containsKey(childValue)) {
+                nodeMap.put(childValue, new TreeNode(childValue));
+            }
+
+            if (isLeft) {
+                nodeMap.get(parentValue).left = nodeMap.get(childValue);
+            } else {
+                nodeMap.get(parentValue).right = nodeMap.get(childValue);
+            }
+
+            children.add(childValue);
+        }
+
+        for (TreeNode node : nodeMap.values()) {
+            if (!children.contains(node.val)) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+}
